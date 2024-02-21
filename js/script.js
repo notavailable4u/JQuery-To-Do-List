65ea878209a893805bd2241f49d3de226d6e519e
notavailable4u <139932183+notavailable4u@users.noreply.github.com>
2024-02-21 08:57:27 -0600

    Update JS script

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 function newItem() {
-   // add new list item
-   let li = $('<li></li>');
-   let list = $('#list');
-   let inputValue = $('#input').val();
-   if (inputValue === '') {
-    alert("You must write something!");
-   } else {
-    li.append(inputValue);
-    list.append(li); 
-   }
+  // add new list item
+  let li = $('<li></li>');
+  let list = $('#list');
+  let inputValue = $('#input').val();
+  if (inputValue === '') {
+   alert("You must write something!");
+  } else {
+   li.append(inputValue);
+   list.append(li); 
+  }
 
 // Toggle crossing out an item from the list of items
 li.on("dblclick", function() {
-    li.toggleClass("strike");
-  });
+   li.toggleClass("strike");
+ });
 
 // Adding the delete button "X":
 let crossOutButton = $('<crossOutButton></crossOutButton>');
@@ -22,13 +22,10 @@ li.append(crossOutButton);
 
 //Adding CLASS DELETE (DISPLAY: NONE) from the css
 crossOutButton.on("click", function() {
-    li.addClass("delete")
+   li.addClass("delete")
 });
 
 //Reorder items
 $('#list').sortable();
 
 }
-
-
-
